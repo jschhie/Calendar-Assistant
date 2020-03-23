d9667ac2946c7dbd62e14b7e248a68c7b3005eb4
Jennifer Chhie <jschhie@ucdavis.edu>
2020-03-22 17:37:11 -0700

    ignore merge problems

--- dayOfWeek.cpp
+++ dayOfWeek.cpp
@@ -1,85 +1,93 @@
+// Authors: Jennifer Chhie and Tammy Lee
 #include <fstream>
 #include <iostream>
 #include <cstring>
 #include <iomanip>
-#include "dayOfWeek.h"
+#include "DayOfWeek.h"
 
 using namespace std;
 
-dayOfWeek::dayOfWeek(int month1, int day1, int year1) : month(month1), 
+
+DayOfWeek::DayOfWeek(int month1, int day1, int year1) : month(month1), 
   day(day1), year(year1)
 {
-} // dayOfWeek() constructor
+}  // DayOfWeek()
 
-
-bool dayOfWeek::operator== (char c) const 
+bool DayOfWeek::operator== (char c) const 
 {
   switch (c)
   {
     case 'M':
       return (strcmp(dayName, "Monday") == 0);
+
     case 'T':
       return (strcmp(dayName, "Tuesday") == 0);
+
     case 'W':
       return (strcmp(dayName, "Wednesday") == 0);
+
     case 'R':
       return (strcmp(dayName, "Thursday") == 0);
+
     case 'F':
       return (strcmp(dayName, "Friday") == 0);
+
     case 'S':
       return (strcmp(dayName, "Saturday") == 0);
+
     case 'U':
       return (strcmp(dayName, "Sunday") == 0);
+
     default: 
       return false;
+
   } // Compares MTWRFSU with dayName
-} // dayOfWeek overloaded operator
 
+} // DayOfWeek overloaded operator
 
-ostream& operator<< (ostream &os, const dayOfWeek &dayOfWeekRef)
+ostream& operator<< (ostream &os, const DayOfWeek &dayOfWeek)
 {
   char line[80], dayStr[3], yearStr[10];
-  strcpy(line, dayOfWeekRef.dayName);
+  strcpy(line, dayOfWeek.dayName);
   strcat(line, ", ");
-  strcat(line, dayOfWeekRef.monthName);
+  strcat(line, dayOfWeek.monthName);
   strcat(line, " ");
     
-  if(dayOfWeekRef.day > 9)
+  if(dayOfWeek.day > 9)
   {
-    dayStr[0] = dayOfWeekRef.day / 10 + '0';
-    dayStr[1] = dayOfWeekRef.day % 10 + '0';
+    dayStr[0] = dayOfWeek.day / 10 + '0';
+    dayStr[1] = dayOfWeek.day % 10 + '0';
     dayStr[2] = '\0';
   } // if day > 9
   else // day < 10
   {
-    dayStr[0] = dayOfWeekRef.day + '0';
+    dayStr[0] = dayOfWeek.day + '0';
     dayStr[1] = '\0';
   } // else day < 10
   
   strcat(line, dayStr);
   strcat(line, ", ");
    
-  int copyYear = dayOfWeekRef.year / 1000;
+  int copyYear = dayOfWeek.year / 1000;
   
   yearStr[0] = copyYear + '0';
-  yearStr[1] = ( (dayOfWeekRef.year / 100) - (copyYear * 10) ) + '0';
-  yearStr[2] = ( (dayOfWeekRef.year / 10 ) % 10) + '0';
-  yearStr[3] = dayOfWeekRef.year % 10 + '0';
+  yearStr[1] = ( (dayOfWeek.year / 100) - (copyYear * 10) ) + '0';
+  yearStr[2] = ( (dayOfWeek.year / 10 ) % 10) + '0';
+  yearStr[3] = dayOfWeek.year % 10 + '0';
   yearStr[4] = '\0';
 
   strcat(line, yearStr);
 
   os << left << setw(30) << line << right;
-
   return os;
+}  // operator<<()
 
-}  // operator<<
 
-
-istream& operator>> (istream &is, dayOfWeek &dayOfWeekPtr) // NOTE: Changed day -> dayOfWeekPtr
+istream& operator>> (istream &is, DayOfWeek &day) 
 {
-  int dateNumber = (dayOfWeekPtr.month - 1) * 31 + dayOfWeekPtr.day - 1 + (dayOfWeekPtr.year - 1990) * 372;
-  is.seekg(dateNumber * sizeof(dayOfWeek));
-  is.read((char*) &dayOfWeekPtr, sizeof(dayOfWeek));
+  int dateNumber = (day.month - 1) * 31 + day.day - 1 + (day.year - 1990) * 372;
+  is.seekg(dateNumber * sizeof(DayOfWeek));
+  is.read((char*) &day, sizeof(DayOfWeek));
   return is;
 }  // operator>>
+
